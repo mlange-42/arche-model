925ad67f8a2cfd9574f873b9fc82f291ea297683
mlange-42 <martin_lange_@gmx.net>
2023-04-04 12:20:30 +0200

    add examples for implementing systems and UI systems

---
+++ model/example_ui_system_test.go
@@ -0,0 +1,46 @@
+package model_test
+
+import (
+	"fmt"
+
+	"github.com/mlange-42/arche-model/model"
+	"github.com/mlange-42/arche-model/system"
+	"github.com/mlange-42/arche/ecs"
+	"github.com/mlange-42/arche/generic"
+)
+
+// TestUISystem is an example for implementing [UISystem].
+type TestUISystem struct {
+	timeRes generic.Resource[model.Time]
+}
+
+// Initialize the system.
+func (s *TestUISystem) InitializeUI(w *ecs.World) {
+	s.timeRes = generic.NewResource[model.Time](w)
+}
+
+// Update the system.
+func (s *TestUISystem) UpdateUI(w *ecs.World) {
+	time := s.timeRes.Get()
+	fmt.Println(time.Tick)
+}
+
+// PostUpdate the system.
+func (s *TestUISystem) PostUpdateUI(w *ecs.World) {}
+
+// Finalize the system.
+func (s *TestUISystem) FinalizeUI(w *ecs.World) {}
+
+func ExampleUISystem() {
+	// Create a new model.
+	m := model.New()
+
+	// Add the test ui system.
+	m.AddUISystem(&TestUISystem{})
+
+	// Add a termination system that ends the simulation.
+	m.AddSystem(&system.FixedTermination{Steps: 30})
+
+	// Run the simulation.
+	m.Run()
+}
