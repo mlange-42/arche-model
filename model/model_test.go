31fa4517f1ea143289b285dec0e69c78a03ccc98
mlange-42 <martin_lange_@gmx.net>
2023-04-10 00:12:27 +0200

    extend Model docs and examples

--- model/model_test.go
+++ model/model_test.go
@@ -35,3 +35,27 @@ func ExampleModel() {
 	m.Run()
 	// Output:
 }
+
+func ExampleModel_Reset() {
+	// Create a new model.
+	m := model.New()
+
+	// Do many model runs.
+	for i := 0; i < 10; i++ {
+		// Reset the model to clear entities, systems etc. before the run.
+		m.Reset()
+
+		// Seed the model for the run.
+		m.Seed(uint64(i))
+
+		// Add systems.
+		m.AddSystem(&system.FixedTermination{
+			Steps: 100,
+		})
+
+		// Run the simulation.
+		m.Run()
+
+	}
+	// Output:
+}
