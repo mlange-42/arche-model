11595cc41e11effcfe702aabe0815102e4551076
Martin Lange <44003176+mlange-42@users.noreply.github.com>
2024-05-23 16:34:40 +0200

    Add `reporter.TableCallback` for direct retrieval of table observer output in Go code (#66)

--- reporter/callback_test.go
+++ reporter/callback_test.go
@@ -10,14 +10,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func ExampleCallback() {
+func ExampleRowCallback() {
 	// Create a new model.
 	m := model.New()
 
 	data := [][]float64{}
 
 	// Add a Print reporter with an Observer.
-	m.AddSystem(&reporter.Callback{
+	m.AddSystem(&reporter.RowCallback{
 		Observer: &ExampleObserver{},
 		Callback: func(step int, row []float64) {
 			data = append(data, row)
@@ -36,13 +36,57 @@ func ExampleCallback() {
 	// [[1 2 3] [1 2 3] [1 2 3]]
 }
 
-func TestCallbackFinal(t *testing.T) {
+func ExampleTableCallback() {
+	// Create a new model.
+	m := model.New()
+
+	data := [][]float64{}
+
+	// Add a Print reporter with an Observer.
+	m.AddSystem(&reporter.TableCallback{
+		Observer: &ExampleSnapshotObserver{},
+		Callback: func(step int, table [][]float64) {
+			data = append(data, table...)
+		},
+		HeaderCallback: func(header []string) {},
+	})
+
+	// Add a termination system that ends the simulation.
+	m.AddSystem(&system.FixedTermination{Steps: 3})
+
+	// Run the simulation.
+	m.Run()
+
+	fmt.Println(data)
+	// Output:
+	// [[1 2 3] [1 2 3] [1 2 3] [1 2 3] [1 2 3] [1 2 3] [1 2 3] [1 2 3] [1 2 3]]
+}
+
+func TestRowCallbackFinal(t *testing.T) {
 	m := model.New()
 	counter := 0
 
-	m.AddSystem(&reporter.Callback{
+	m.AddSystem(&reporter.RowCallback{
 		Observer: &ExampleObserver{},
 		Callback: func(step int, row []float64) {
+			counter++
+		},
+		HeaderCallback: func(header []string) {},
+		Final:          true,
+	})
+	m.AddSystem(&system.FixedTermination{Steps: 3})
+	m.Run()
+
+	assert.Equal(t, 1, counter)
+}
+
+func TestTableCallbackFinal(t *testing.T) {
+	m := model.New()
+	counter := 0
+
+	m.AddSystem(&reporter.TableCallback{
+		Observer: &ExampleSnapshotObserver{},
+		Callback: func(step int, table [][]float64) {
 			counter++
 		},
 		HeaderCallback: func(header []string) {},
